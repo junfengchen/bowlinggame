41143e07ea7dbf007e2d2752ec9498e525f079a5
Jeffer Chen <jeffer@supersupport.com.au>
2019-10-31 15:19:09 +1100

    roll test and validation

--- src/BowlingGame.js
+++ src/BowlingGame.js
@@ -1,6 +1,39 @@
+/***
+ * @description Bowling Game
+ * @author Jeffer Chen
+ */
+
+export const FRAME_NOT_STARTED = -1
+
 export class BowlingGame{
     constructor(){
         this.nScore = 0
+        this.nPrePins = FRAME_NOT_STARTED
+    }
+
+    roll(noOfPins){
+        let bValid = false;
+        if(noOfPins>=0 && noOfPins<=10)
+            bValid = true;
+
+        if(this.newFrame()){
+            this.nPrePins = noOfPins
+            if(noOfPins===10)
+                this.nPrePins = FRAME_NOT_STARTED
+        }else{
+            const pinsThisFrame = this.nPrePins + noOfPins
+            this.nPrePins = FRAME_NOT_STARTED
+            if(pinsThisFrame>=0 && pinsThisFrame<=10){
+                bValid = true
+            }else{
+                bValid = false
+            }
+        }
+        return bValid
+    }
+
+    newFrame(){
+        return (this.nPrePins === FRAME_NOT_STARTED)
     }
 
     score(){
--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@
  */
 
 import { BowlingGame } from '../src/BowlingGame'
-import { expect } from 'chai'
+import { expect, assert } from 'chai'
 
 
 describe("init", () => {
@@ -12,5 +12,32 @@ describe("init", () => {
         const bowlingGame = new BowlingGame()
         const score = bowlingGame.score()
         expect(score).to.be.equals(0);
+    })
+})
+
+
+describe("roll", () => {
+    it("should allow bowler knocking down certain number(between 0 and 10) of pins", ()=>{
+        const bowlingGame = new BowlingGame()
+        assert.equal(bowlingGame.roll(5), true)
+    })
+
+    it("should return false if number of pins is not in the range", ()=>{
+        const bowlingGame = new BowlingGame()
+        assert.equal(bowlingGame.roll(11), false)
+
+        const bowlingGame2 = new BowlingGame()
+        assert.equal(bowlingGame2.roll(-1), false)
+    })
+
+    it("should allow max 10 pins in each frame (2 tries)", ()=>{
+        const bowlingGame = new BowlingGame()
+        assert.equal(bowlingGame.roll(3), true)
+        assert.equal(bowlingGame.roll(7), true)
+
+        const bowlingGame2 = new BowlingGame()
+        assert.equal(bowlingGame2.roll(7), true)
+        // try to go over 10 pins in the 2nd try
+        assert.equal(bowlingGame2.roll(7), false)
     })
 })
